5b95d31ac475f3f69933a665aad5c40a91d003fa
Marina Portugal <marinapsvreis@gmail.com>
2022-07-16 18:03:42 -0300

    tema escuro

---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,43 @@
+import React, { useState, createContext, useCallback, useMemo, useContext } from "react";
+import { ThemeProvider } from "@mui/material";
+import { DarkTheme } from "../themes/DarkTheme";
+import { LightTheme } from "../themes/LightTheme";
+import { Box } from "@mui/system";
+
+
+interface IThemeContextData {
+  themeName: 'light' | 'dark';
+  toogleTheme: () => void;
+}
+
+interface IAppThemeProviderProps {
+  children: React.ReactNode;
+}
+
+const ThemeContext = createContext({} as IThemeContextData);
+
+export const useAppThemeContext = () => {
+  return useContext(ThemeContext);
+}
+
+export const AppThemeProvider: React.FC<IAppThemeProviderProps> = ({ children }) => {
+
+  const [themeName, setThemeName] = useState<'light' | 'dark'>('light');
+  const toogleTheme = useCallback(() => {
+    setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light');
+  }, []);
+  const theme = useMemo(() => {
+    if (themeName === 'light') return LightTheme;
+    return DarkTheme;
+  }, [themeName]);
+
+  return (
+    <ThemeContext.Provider value={{ themeName, toogleTheme }}>
+      <ThemeProvider theme={theme}>
+        <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
+          {children}
+        </Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  )
+}
